f6443cbbb205421de79c9f76230b7b836de77323
ratchanon2318 <ratchanon.noknoy2318@gmail.com>
2026-01-25 08:13:53 +0700

    add SQL scripts for HOSxP data extraction

--- extract_village_list.sql
+++ extract_village_list.sql
@@ -1,6 +1,6 @@
 SELECT
     vl.village_moo,
-    vl.village_name,
-    ta.full_name
+    CONCAT(LEFT(vl.village_name, 3), '****') AS village_name,
+    CONCAT('****', RIGHT(ta.full_name, 10)) AS full_name
 FROM village vl
 LEFT JOIN thaiaddress ta ON ta.addressid = vl.address_id;
