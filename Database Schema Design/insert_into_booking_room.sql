05ab8792d258c51a9b622c5701a4639dfa996853
ratchanon2318 <ratchanon.noknoy2318@gmail.com>
2026-01-27 15:10:30 +0700

    rm Database

--- Database Schema Design/insert_into_booking_room.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO `room` (`room_name`, `room_detail`, `capacity`, `status`) VALUES
-('Meeting Room A', 'Projector, Whiteboard, 10 Chairs', 10, 'Available'),
-('Meeting Room B', 'TV Screen, Conference Phone, 8 Chairs', 8, 'Available'),
-('Grand Hall', 'Stage, Sound System, 100 Seats', 100, 'Available'),
-('Focus Room 1', 'Small desk, 2 Chairs', 2, 'Maintenance');
