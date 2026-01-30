7ede8517a739d3fdcd11b3801331f8273c795a49
Ratchanon Noknoy <ratchanon.noknoy2318@gmail.com>
2026-01-30 17:34:02 +0700

    Update telemedicine_completed.sql

--- SQL/telemedicine_completed.sql
+++ SQL/telemedicine_completed.sql
@@ -1,10 +1,24 @@
-SELECT o.hn, CONCAT(p.pname, p.fname, ' ', p.lname) AS full_name, o.vstdate, vnstat.age_y, pt.name, ic.tname, vnstat.income
+SELECT
+    o.hn,
+    CONCAT(p.pname, p.fname, ' ', p.lname) AS full_name,
+    o.vstdate,
+    vn.age_y,
+    pt.name AS pttype_name,
+    ic.tname AS pdx_name,
+    vn.income
 FROM ovst o
-    LEFT JOIN patient p ON o.hn = p.hn
-    LEFT JOIN opitemrece oi ON o.vn = oi.vn
-    LEFT JOIN nondrugitems nd ON oi.icode = nd.icode
-    left join vn_stat vnstat on vnstat.vn = o.vn
-    left join pttype pt on pt.pttype = vnstat.pttype
-    left join icd101 ic on vnstat.pdx = ic.code
-where oi.icode = '3003921' and o.vstdate between '2025-01-01' and '2025-06-16'
-order by o.vstdate
+JOIN opitemrece oi
+    ON o.vn = oi.vn
+JOIN nondrugitems nd
+    ON oi.icode = nd.icode
+LEFT JOIN patient p
+    ON o.hn = p.hn
+LEFT JOIN vn_stat vn
+    ON vn.vn = o.vn
+LEFT JOIN pttype pt
+    ON pt.pttype = vn.pttype
+LEFT JOIN icd101 ic
+    ON vn.pdx = ic.code
+WHERE oi.icode = '3003921'
+  AND o.vstdate BETWEEN '2025-01-01' AND '2025-06-16'
+ORDER BY o.vstdate;
