0b2d82ce2b0c23749d1eb8f3715af2f63fce33c9
Ratchanon Noknoy <ratchanon.noknoy2318@gmail.com>
2026-01-30 17:38:11 +0700

    Update extract_patient_drug_allergy.sql

--- SQL/extract_patient_drug_allergy.sql
+++ SQL/extract_patient_drug_allergy.sql
@@ -6,13 +6,19 @@ SELECT
     CONCAT(p.pname, p.fname, ' ', p.lname) AS patient_name,
     ar.relation_name,
     ag.allergy_group_name,
-    asr.seriousness_name,
-    ars.result_name,
+    srs.seriousness_name,
+    arl.result_name,
     sp.name AS spclty_name
 FROM opd_allergy o
-LEFT OUTER JOIN patient p ON p.hn = o.hn
-LEFT OUTER JOIN allergy_group ag ON ag.allergy_group_id = o.allergy_group_id
-LEFT OUTER JOIN allergy_seriousness asr ON asr.seriousness_id = o.seriousness_id
-LEFT OUTER JOIN allergy_result ars ON ars.allergy_result_id = o.allergy_result_id
-LEFT OUTER JOIN allergy_relation ar ON ar.allergy_relation_id = o.allergy_relation_id
-LEFT OUTER JOIN spclty sp ON sp.spclty = o.spclty;
+LEFT JOIN patient p
+    ON p.hn = o.hn
+LEFT JOIN allergy_group ag
+    ON ag.allergy_group_id = o.allergy_group_id
+LEFT JOIN allergy_seriousness srs
+    ON srs.seriousness_id = o.seriousness_id
+LEFT JOIN allergy_result arl
+    ON arl.allergy_result_id = o.allergy_result_id
+LEFT JOIN allergy_relation ar
+    ON ar.allergy_relation_id = o.allergy_relation_id
+LEFT JOIN spclty sp
+    ON sp.spclty = o.spclty;
