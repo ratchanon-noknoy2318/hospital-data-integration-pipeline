48b39ffa25a59507c6e8df0e1951366f4053e2a0
Ratchanon Noknoy <ratchanon.noknoy2318@gmail.com>
2026-01-30 17:33:30 +0700

    Update extract_patient_billing_by_group.sql

--- SQL/extract_patient_billing_by_group.sql
+++ SQL/extract_patient_billing_by_group.sql
@@ -6,9 +6,12 @@ SELECT
     nd.icode,
     nd.name AS service_name
 FROM ovst o
-LEFT JOIN patient p ON o.hn = p.hn
-LEFT JOIN opitemrece oi ON o.vn = oi.vn
-LEFT JOIN nondrugitems nd ON oi.icode = nd.icode
+JOIN opitemrece oi 
+    ON o.vn = oi.vn
+JOIN nondrugitems nd 
+    ON oi.icode = nd.icode
+LEFT JOIN patient p 
+    ON o.hn = p.hn
 WHERE oi.icode = '3003921'
-    AND o.vstdate BETWEEN '2025-01-01' AND '2025-06-16'
-ORDER BY o.vstdate;
+  AND o.vstdate BETWEEN '2025-01-01' AND '2025-06-16'
+ORDER BY o.vstdate, o.vsttime;
